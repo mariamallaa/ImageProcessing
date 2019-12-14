16ad48d80b4dee0fe30e9cfa02deac3c360d4fd4
mariamallaa <37220622+mariamallaa@users.noreply.github.com>
2019-12-14 12:54:18 +0200

    skinSegmentation

---
+++ projectimageskinsegmentation/appliedfunctions.py
@@ -0,0 +1,232 @@
+from commonfunctions import *
+from skimage.filters import gaussian 
+from skimage.color import rgb2ycbcr
+from skimage.color import rgb2yuv
+from skimage.transform import resize
+import numpy as np
+import math
+from skimage.morphology import binary_erosion, binary_dilation
+from skimage import exposure
+import cv2
+from skimage.filters import threshold_otsu
+from skimage.transform import rotate
+
+
+######################################################################################################
+######################################################################################################
+######################################################################################################
+
+def erode(img, window_size, origin_position):
+    new_img = np.ones((img.shape[0], img.shape[1]))
+    for i in range(origin_position[0], img.shape[0] - window_size[0] + origin_position[0]):
+        for j in range(origin_position[1], img.shape[1] - window_size[1] + origin_position[1]):
+            x_start = i-origin_position[0]
+            y_start = j-origin_position[1]
+            mini = 256
+            for x in range( x_start , x_start + window_size[0]):
+                for y in range( y_start, y_start + window_size[1]):
+                    if img[x][y] < mini:
+                        mini = img[x][y]
+                        
+            if(x_start + window_size[0]+1 < img.shape[0] and img[x_start + window_size[0]+1][y_start]<mini):
+                mini = img[x_start + window_size[0]+1][y_start]
+            if(y_start + window_size[1]+1 < img.shape[1] and img[x_start ][y_start+ window_size[1]+1]<mini):
+                mini = img[x_start ][y_start+ window_size[1]+1]
+                
+            if(x_start + window_size[0]-1 > 0 and img[x_start + window_size[0]-1][y_start]<mini):
+                mini = img[x_start + window_size[0]-1][y_start]
+            if(y_start + window_size[1]-1 > 0 and img[x_start ][y_start+ window_size[1]-1]<mini):
+                mini = img[x_start ][y_start+ window_size[1]-1]
+            
+            new_img[i,j] = mini 
+    return new_img
+
+
+
+def erodesmall(img, window_size, origin_position):
+    new_img = np.ones((img.shape[0], img.shape[1]))
+    for i in range(origin_position[0], img.shape[0] - window_size[0] + origin_position[0]):
+        for j in range(origin_position[1], img.shape[1] - window_size[1] + origin_position[1]):
+            x_start = i-origin_position[0]
+            y_start = j-origin_position[1]
+            mini = 256
+            for x in range( x_start , x_start + window_size[0]):
+                for y in range( y_start, y_start + window_size[1]):
+                    if img[x][y] < mini:
+                        mini = img[x][y]
+            
+            new_img[i,j] = mini 
+    return new_img
+
+
+
+def dilate(img, window_size, origin_position):
+    new_img = np.ones((img.shape[0], img.shape[1]))
+    for i in range(origin_position[0], img.shape[0] - window_size[0] + origin_position[0]):
+        for j in range(origin_position[1], img.shape[1] - window_size[1] + origin_position[1]):
+            x_start = i-origin_position[0]
+            y_start = j-origin_position[1]
+            maxi = -1
+            for x in range( x_start , x_start + window_size[0]):
+                for y in range( y_start, y_start + window_size[1]):
+                    if img[x][y] > maxi:
+                        maxi = img[x][y]
+            if(x_start + window_size[0]+1 < img.shape[0] and img[x_start + window_size[0]+1][y_start]>maxi):
+                maxi = img[x_start + window_size[0]+1][y_start]
+            if(y_start + window_size[1]+1 < img.shape[1] and img[x_start ][y_start+ window_size[1]+1]>maxi):
+                   maxi = img[x_start ][y_start+ window_size[1]+1]
+                
+            if(x_start + window_size[0]-1 > 0 and img[x_start + window_size[0]-1][y_start]>maxi):
+                maxi = img[x_start + window_size[0]-1][y_start]
+            if(y_start + window_size[1]-1 > 0 and img[x_start ][y_start+ window_size[1]-1]>maxi):
+                maxi = img[x_start ][y_start+ window_size[1]-1]
+            new_img[i,j] = maxi
+    return new_img
+
+
+def distance(img, ic,jc):
+    arr=[]
+    for i in range(25,ic):
+        for j in range(30,jc):
+            if(img[i][j]==1):
+                dist= math.sqrt((i-ic)**2 + (j-jc)**2)
+                for n in range(25,175):
+                    for m in range(jc,150):
+                        if(img[n][m]==1):
+                            dist2= math.sqrt((n-ic)**2 + (m-jc)**2)
+                            if(abs(dist-dist2)/dist<30/100):
+                                if(abs(i-n)<20):
+                                    if(abs(j-m)<80):
+                                        arr.append([i,j,n,m])                                
+    arrnp =np.asarray(arr)
+    
+    return arrnp
+                    
+
+
+def sunglassesfilter(img,midpointx,midpointy,h,w,degree):
+            sunglass_image = io.imread('sun.jpg')
+
+
+            resized_sunglass=resize(sunglass_image[0:600,:],(100,150))
+            print(degree)
+            if(degree!=0):
+                resized_sunglass=rotate(resized_sunglass, degree,cval=1)
+            show_images([resized_sunglass])
+
+            resized_sunglass[resized_sunglass>=0.99607843]=1
+            resized_sunglass[resized_sunglass<0.99607843]=0
+            
+            for i in range(resized_sunglass.shape[0]):
+                for j in range(resized_sunglass.shape[1]):
+                    if resized_sunglass[i,j,1]==0:
+                        img[midpointx-50+i,midpointy-75+j]=(resized_sunglass[i,j])
+               
+            img=resize(img,(h,w))
+            
+            finalimg=(img*255).astype('uint8')
+            return finalimg
+
+def hatfilter(img,h,w,degree):
+            hat_image = io.imread('hat2.jpg')
+
+
+            resized_hat=resize(hat_image[0:700,:],(100,160))
+            
+            show_images([resized_hat])
+            
+            if(degree!=0):
+                resized_hat=rotate(resized_hat, degree,cval=1)
+               
+            resized_hat2=np.copy(resized_hat)
+            resized_hat[resized_hat>=0.7]=1
+            resized_hat[resized_hat<0.7]=0
+            
+            for i in range(resized_hat.shape[0]):
+                for j in range(resized_hat.shape[1]):
+                    if resized_hat[i,j,1]==0:
+                        img[i,j+20]=(resized_hat2[i,j])
+            img=resize(img,(h+20,w))
+            
+            finalimg=(img*255).astype('uint8')
+            return finalimg
+
+def clown_nose_filter(img,nosex,nosey,h,w,degree):
+        clown_nose= io.imread('clown-nose.jpg')
+        resized_clown_nose=resize(clown_nose[0:500,0:500],(70,100))
+        if(degree!=0):
+                resized_clown_nose=rotate(resized_clown_nose, degree,cval=1)
+        resized_clown_nose[resized_clown_nose>=0.7]=1
+        resized_clown_nose[resized_clown_nose<0.7]=0
+
+        for i in range(resized_clown_nose.shape[0]):
+            for j in range(resized_clown_nose.shape[1]):
+                if resized_clown_nose[i,j,1]==0:
+                    img[nosex-30+i,nosey-50+j]=(resized_clown_nose[i,j])
+
+
+        img=resize(img,(h,w))
+            
+        finalimg=(img*255).astype('uint8')
+        return finalimg
+    
+
+
+def geteyemap(img):
+        ycbcr_image = rgb2ycbcr(img).astype('uint8')
+        y = ycbcr_image[:,:,0]
+        cb = ycbcr_image[:,:,1]
+        cr = ycbcr_image[:,:,2]
+
+        chromamap= 1/3 *((cb/cr) + cb**2 + (1-cr)**2)
+
+        newchromamap = exposure.equalize_hist(chromamap)
+
+        windowsize=[3,3]
+        orgin=[1,1]
+        ydilate= dilate(y,windowsize,orgin)
+        yerode= erode(y,windowsize,orgin)
+
+        lumamap = ydilate / (yerode+1)
+
+
+        eyemap =(1-newchromamap)+lumamap
+        return eyemap
+
+def getnose(midpointx,midpointy,righti,lefti):
+    nosex = int((midpointx) *1.5)
+    if(righti>lefti and righti-lefti>10):
+        nosey=int(midpointy*1.2)
+    elif(righti<lefti and lefti-righti>10):
+        nosey=int(midpointy*0.8)
+    else:
+        nosey = midpointy
+    return nosex,nosey
+
+
+
+'''
+def skinsegmentation(imgRGB,imgHSV,imgYCbCr):
+    ##RGB
+    R = imgRGB[:,:,0]
+    G = imgRGB[:,:,1]
+    B = imgRGB[:,:,2]
+    ##HSV
+    H = imgHSV[:,:,0]
+    S = imgHSV[:,:,1]
+    V = imgHSV[:,:,2]
+    ##YCbCr
+    y = imgYCbCr[:,:,0]
+    cb = imgYCbCr[:,:,1]
+    cr = imgYCbCr[:,:,2]
+
+    (R>50) && (G>40) && (B>20) && [max{max(R,G),B}– min{min(R,G),B}]>10) && (R – G >= 10) && (R>G) && (R>B)
+    Rmax=np.max(R)
+    Gmax=np.max(G)
+    Bmax=np.max(B)
+    eq1=imgRGB[R>50 and G>40 and B>20 and ]
+
+'''
+######################################################################################################
+######################################################################################################
+######################################################################################################
---
+++ projectimageskinsegmentation/filters.py
@@ -0,0 +1,180 @@
+from commonfunctions import *
+from appliedfunctions import *
+from skinseg import *
+from skimage.filters import gaussian 
+from skimage.color import rgb2ycbcr
+from skimage.transform import resize
+import numpy as np
+import math
+from skimage.morphology import binary_erosion, binary_dilation
+from skimage import exposure
+from skimage.morphology import erosion, dilation
+from skimage.morphology import disk
+import cv2
+
+######################################################################################################
+######################################################################################################
+######################################################################################################
+windowsize=[3,3]
+orgin=[1,1]
+Structure = disk(2)
+
+
+
+######################################################################################################
+######################################################################################################
+######################################################################################################
+
+
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
+
+#creating adaboost trained model using cv2
+#face_cascade = cv2.CascadeClassifier('C:\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml')
+
+#val = input("press 1 for camera image press 2 for choosing an image from your computer: ")
+
+while True:
+    #reading frame from camera
+    #ret, frame = cam.read()
+    #cv2.imshow("test", frame)
+    #img =frame
+    #img = img[:, :, ::-1]
+
+    #reading an exisiting image
+
+    img =  io.imread("mirna.jpg").astype('uint8')
+    show_images([img])
+
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    #faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = getfaces(img)
+    hi=1
+
+    for (x,yt,w,h) in faces:
+        cropped_img = img[yt:yt+h,x:x+w ,:]
+        cropped_img2=img[yt-20:yt+h,x:x+w ,:]
+        show_images([cropped_img,cropped_img2])
+        
+        if(hi==1):
+            hi=2
+            continue
+        if(hi==2):
+            hi=3
+            continue
+        if(hi==3):
+            hi=4
+            continue
+        
+        #print("x,yt")
+        #crop the face
+        cropped_img = img[yt:yt+h,x:x+w ,:]
+        cropped_img2=img[yt-20:yt+h,x:x+w ,:]
+        show_images([cropped_img,cropped_img2])
+        #removing noise
+
+        filtered_img = gaussian(cropped_img, sigma=0.1,multichannel=False)
+        filtered_img = gaussian(cropped_img2, sigma=0.1,multichannel=False)
+        #resize image
+
+        resized_image=resize(cropped_img,(200,180))
+        resized_image2=resize(cropped_img2,(200,180))
+        show_images([resized_image2])
+        #finding the eyemap
+        
+        
+        eye_location= np.array([])
+       
+        factor=0.7
+        while eye_location.size==0:
+            eyemap=geteyemap(resized_image)
+            
+            thresh =np.max(eyemap)*factor
+            
+            
+            eyemap[eyemap<thresh]=0
+            eyemap[eyemap>thresh]=1
+           
+
+            show_images([resized_image,eyemap])
+
+            
+            #eyemap = erosion(eyemap, Structure)
+            #eyemap = dilation(eyemap, Structure)
+            #eyemap= erode(eyemap,windowsize,orgin)
+            #eyemap= dilate(eyemap,windowsize,orgin)
+                
+           # eyemap= erodesmall(eyemap,windowsize,orgin)
+                
+            show_images([resized_image,eyemap])
+            eye_location=distance(eyemap, 125,90)
+            factor-=0.1
+
+        #print(eye_location)
+        #print(eye_location.count)
+        show_images([resized_image,eyemap])
+
+        righti = np.mean(eye_location[:,0])
+        rightj = np.mean(eye_location[:,1])
+        lefti = np.mean(eye_location[:,2])
+        leftj = np.mean(eye_location[:,3])
+        eyearr=np.asarray([righti,rightj,lefti,leftj])
+        print(righti)
+        print(rightj)
+        print(lefti)
+        print(leftj)
+        img_eyes=resized_image[int(eyearr[2]-10):int(eyearr[0]+10),int(eyearr[1]-20):int(eyearr[3]+20)]
+        show_images([img_eyes])
+        degree=0
+        print(righti)
+        print(lefti)
+        if(righti>lefti and righti-lefti>10):
+            degree=20
+        elif(righti<lefti and lefti-righti>10):
+            degree=-20
+
+        #print("mirna")
+        #print(eyearr)
+        eyearr=eyearr.astype('uint16')
+        
+        new_croppedimg=np.zeros(eyemap.shape)
+
+        new_croppedimg[eyearr[0],eyearr[1]]=1
+
+        new_croppedimg[eyearr[2],eyearr[3]]=1
+        
+        
+        midpointx= int((eyearr[0]+eyearr[2])/2)
+        midpointy= int((eyearr[1]+eyearr[3])/2)
+        #nose
+        image_nose=resized_image[int(eyearr[0]):int(eyearr[0]*2),int(eyearr[1]):int(eyearr[3])]
+        print("Nosess")
+        show_images([image_nose])
+        nosex,nosey= getnose(midpointx,midpointy,righti,lefti)
+
+        new_croppedimg[nosex,nosey]=1
+        print("NOSE")
+        show_images([new_croppedimg])
+        img[yt:yt+h,x:x+w ,:]=sunglassesfilter(resized_image,midpointx,midpointy,h,w,degree)
+        show_images([img])
+        img[yt:yt+h,x:x+w,:]=clown_nose_filter(resized_image,nosex,nosey,h,w,degree)
+        show_images([img])
+        img[yt-20:yt+h,x:x+w,:]=hatfilter(resized_image2,h,w,degree)
+        cv2.imshow("test", img)
+
+        show_images([img])
+
+
+
+    cv2.imshow('img',img)
+   
+    #print(img.shape)
+    #print(img)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    
